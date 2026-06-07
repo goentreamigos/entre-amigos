510ac8d3d544bc57cb3357d4396441af37916fd0
goentreamigos <goentreamigos@gmail.com>
2026-06-07 15:07:00 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Tells Tailwind to scan these files for class names
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
